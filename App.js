32b9d2f15888e3e3534b6e56d123e75601cd6e5c
Nikhil Kumar Singh <nikhilsinghns01@gmail.com>
2021-12-26 19:17:06 +0530

    initial stage

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
+import AppLoading from 'expo-app-loading';
+
+import MealsNavigator from './navigation/MealsNavigator';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
+  return <MealsNavigator />;
+}
